b0b9706c73b15587752fe692a59ea9e1d855954f
yusukesaitoh <saitoh@magiclogic.com>
2020-05-23 10:50:37 +0900

    tsを整形

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -2,34 +2,33 @@ toggleClass(["isExploreHidden", "isTrendsHidden", "isReactionNumberHidden", "sho
 addCalmTitle();
 
 function toggleClass(keys: string[]) {
-    chrome.storage.local.get(keys, function(data) {
-        keys.forEach(key => {
             if (typeof data[key] === "undefined") {
                 data[key] = true;
             }
-            let body = document.getElementsByTagName('body')[0];
-
-            if (data[key]) {
-                body.classList.add(key);
-            } else {
-                body.classList.remove(key);
-            }
-        });
+  chrome.storage.local.get(keys, function (data) {
+    keys.forEach(key => {
+      let body = document.getElementsByTagName('body')[0];
+      if (data[key]) {
+        body.classList.add(key);
+      } else {
+        body.classList.remove(key);
+      }
     });
+  });
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    toggleClass([request.key]);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  toggleClass([request.key]);
 });
 
 chrome.runtime.sendMessage({ from: 'content', subject: 'showPageAction' });
 
 function addCalmTitle() {
-    let calmText = chrome.i18n.getMessage("textCalm");
-    let css = "body.showCalmText header[role=\"banner\"] h1[role=\"heading\"]::after { content:\"" + calmText +  "\"; }";
-    let head = document.head || document.getElementsByTagName('head')[0];
-    let style = document.createElement('style');
-    head.appendChild(style);
-    style.type = 'text/css';
-    style.appendChild(document.createTextNode(css));
+  let calmText = chrome.i18n.getMessage("textCalm");
+  let css = "body.showCalmText header[role=\"banner\"] h1[role=\"heading\"]::after { content:\"" + calmText + "\";}";
+  let head = document.head || document.getElementsByTagName('head')[0];
+  let style = document.createElement('style');
+  head.appendChild(style);
+  style.type = 'text/css';
+  style.appendChild(document.createTextNode(css));
 }
--- app/scripts/popup.ts
+++ app/scripts/popup.ts
@@ -2,37 +2,35 @@ localize();
 initValues(["isExploreHidden", "isTrendsHidden", "isReactionNumberHidden", "showCalmText"]);
 
 function localize() {
-    var objects = document.getElementsByTagName('html');
-    for (var j = 0; j < objects.length; j++)
-    {
-        var obj = objects[j];
+  var objects = document.getElementsByTagName('html');
+  for (var j = 0; j < objects.length; j++) {
+    var obj = objects[j];
 
-        var valStrH = obj.innerHTML.toString();
-        var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1)
-        {
-            return v1 ? chrome.i18n.getMessage(v1) : "";
-        });
+    var valStrH = obj.innerHTML.toString();
+    var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function (match, v1) {
+      return v1 ? chrome.i18n.getMessage(v1) : "";
+    });
 
-        if(valNewH != valStrH)
-        {
-            obj.innerHTML = valNewH;
-        }
+    if (valNewH != valStrH) {
+      obj.innerHTML = valNewH;
     }
+  }
+
 }
 
 function addClickEventListeners(keys: string[]) {
-    keys.forEach(key => {
-        var input = document.getElementById(key);
-        input!.addEventListener('click', function(event) {
-            var checkbox = event.target as HTMLInputElement;
-            chrome.storage.local.set({[key] : checkbox!.checked}, function () {});
+  keys.forEach(key => {
+    var input = document.getElementById(key);
+    input!.addEventListener('click', function (event) {
+      var checkbox = event.target as HTMLInputElement;
+      chrome.storage.local.set({ [key]: checkbox!.checked }, function () { });
 
-            chrome.tabs.query( {active: true, currentWindow: true}, function(tabs){
-                let tabId = tabs[0].id;
-                chrome.tabs.sendMessage(tabId!, {key: key}, function(){});
-            });
-        }, false);
-    });
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let tabId = tabs[0].id;
+        chrome.tabs.sendMessage(tabId!, { key: key }, function () { });
+      });
+    }, false);
+  });
 }
 
 function toggleChecked(keys: string[]) {
@@ -49,6 +47,6 @@ function toggleChecked(keys: string[]) {
 }
 
 function initValues(keys: string[]) {
-    toggleChecked(keys);
-    addClickEventListeners(keys);
+  toggleChecked(keys);
+  addClickEventListeners(keys);
 }
