1e671b64cc190f2fdbb388ba0fc8d181542589fa
Yusuke Saitoh <saitoh@magiclogic.jp>
2022-12-24 17:19:39 +0900

    アクションをdisableにする処理をonStartupに移動

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -1,4 +1,7 @@
-chrome.action.disable();
+chrome.runtime.onStartup.addListener(function () {
+  chrome.action.disable();
+});
+
 chrome.runtime.onMessage.addListener((request, sender) => {
   if ((request.from === 'content') && (request.subject === 'showPageAction')) {
     chrome.action.enable(sender.tab!.id!);
